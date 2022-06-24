c68f8556342bc8c72ce67dd659e656aa716e7062
Matthew McGrath <rubywep@hotmail.com>
2022-06-23 23:45:44 -0700

    Finish Destination and Crew pages
    Made the image of a Destination panel hide/appear appropriately depending on which panel is currently active. Applied the same logic of the tab system on the Destination page to the Crew page, allowing users to navigate through the different crew member panels.
    
    Issue: For some reason, the crew member images do not get hidden when we navigate away from their panel. I will fix this ASAP!

--- tabs.js
+++ tabs.js
@@ -15,40 +15,54 @@ function changeTabFocus(event) {
 
   if (event.keyCode === keydownLeft || event.keyCode === keydownRight) {
     tabs[activeTabIndex].setAttribute('tabindex', -1);
-  }
 
-  // Check if ta left or right were pressed (37, 39 keycode)
-  if (event.keyCode === keydownLeft) {
-    activeTabIndex--;
-    // Move to right end if we move too far to the left
-    if (activeTabIndex < 0) {
-      activeTabIndex = tabs.length - 1;
+    // Check if ta left or right were pressed (37, 39 keycode)
+    if (event.keyCode === keydownLeft) {
+      activeTabIndex--;
+      // Move to right end if we move too far to the left
+      if (activeTabIndex < 0) {
+        activeTabIndex = tabs.length - 1;
+      }
+    } else if (event.keyCode === keydownRight) {
+      // Keep index between 0 and tab length
+      activeTabIndex = (activeTabIndex + 1) % tabs.length;
     }
-  } else if (event.keyCode === keydownRight) {
-    // Keep index between 0 and tab length
-    activeTabIndex = (activeTabIndex + 1) % tabs.length;
-  }
 
-  tabs[activeTabIndex].setAttribute('tabindex', 0);
-  tabs[activeTabIndex].focus();
+    tabs[activeTabIndex].setAttribute('tabindex', 0);
+    tabs[activeTabIndex].focus();
+  }
 }
 
 function changeTabPanel(event) {
-    
-  event.target.classList.add('active');
-
   const targetTab = event.target;
   const targetPanel = targetTab.getAttribute('aria-controls');
+  const targetImage = targetTab.getAttribute('data-image');
 
   const tabContainer = targetTab.parentNode; // The tab list
   const mainContainer = tabContainer.parentNode; // Get the main element
 
-  console.log(targetTab);
-  console.log(targetPanel);
+  // Set aria-selected to false on the previously-selected tab
+  tabContainer.querySelector('[aria-selected="true"').setAttribute('aria-selected', false);
+  // Set aria-selected to true on current tab, and thus give it active styling
+  targetTab.setAttribute('aria-selected', true);
 
   // Hide every panel's contents
-  mainContainer.querySelectorAll('[role="tabpanel"').forEach((panel) => panel.setAttribute('hidden', true));
-
+  hideContent(mainContainer, '[role="tabpanel"');
   // But show the now-selected panel's contents
-  mainContainer.querySelector(`#${targetPanel}`).removeAttribute('hidden');
+  showContent(mainContainer, `#${targetPanel}`);
+
+  // Hide every panel's picture
+  hideContent(mainContainer, `#${targetImage}`);
+  // But show the now-selected panel's image
+  showContent(mainContainer, `#${targetImage}`);
+
+  console.log('TARGET IMAGE', targetImage);
+}
+
+function hideContent(parent, content) {
+  parent.querySelectorAll(content).forEach((item) => item.setAttribute('hidden', true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute('hidden');
 }
